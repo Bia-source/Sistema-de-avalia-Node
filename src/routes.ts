c638c0b3b3fa66dc29be5818baac72c592cc7590
Beatriz Ferreira dos Santos <bfsantos@RDM503728.local>
2021-08-08 21:42:22 -0300

    buscando avaliação por id

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,8 @@ import { ensureAuthenticate } from "./middlewares/ensureAuthenticated";
 import { ListUserReceiverComplimentsController } from "./controllers/ListUserReceiverComplimentsController";
 import { ListUserSenderComplimentsController } from "./controllers/ListUserSenderComplimentsController";
 import { ListTagController } from "./controllers/ListTagController";
+import { ComplimentByIdController } from "./controllers/ComplimentByIdController";
+
 
 const router = Router();
 const userController = new CreateUserController();
@@ -18,6 +20,7 @@ const complimentController = new CreateComplimentController();
 const listUserReceiverComplimentsController = new ListUserReceiverComplimentsController();
 const listUserSenderComplimentsController = new ListUserSenderComplimentsController();
 const listTagController = new ListTagController();
+const complimentById = new ComplimentByIdController();
 
 router.post("/users",  userController.handle);
 router.post("/tags", ensureAuthenticate, ensureAdmin, tagController.handle);
@@ -26,5 +29,6 @@ router.post("/compliments",  ensureAuthenticate, complimentController.handle);
 router.get("/users/compliments/sender", ensureAuthenticate, listUserSenderComplimentsController.handle);
 router.get("/users/compliments/receiver", ensureAuthenticate, listUserReceiverComplimentsController.handle);
 router.get("/tags/all", listTagController.handle);
+router.get("/users/compliment/:id", complimentById.handle);
 
 export { router }
