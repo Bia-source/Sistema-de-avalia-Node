20da96acda87cd7c610fca5c6c0643617bbd80e6
Beatriz Ferreira dos Santos <bfsantos@RDM503728.local>
2021-08-09 02:54:41 -0300

    ajustando response de acordo com status de amd all receiver

--- src/controllers/ListUserReceiverComplimentsController.ts
+++ src/controllers/ListUserReceiverComplimentsController.ts
@@ -3,10 +3,17 @@ import { ListUserReceiverComplimentsService } from "../services/ListUserReceiver
 
 class ListUserReceiverComplimentsController{
    async handle(request:Request, response:Response){
-       const {user_id} = request;
+       const {user_id, admin} = request;
        const listUserReceiveService = new ListUserReceiverComplimentsService();
+       if(admin === true){
+        const compliments = await listUserReceiveService.execute(user_id,admin);
+        return response.json({listReceive: compliments});
+       }
        const compliments = await listUserReceiveService.execute(user_id);
        return response.json({listReceive: compliments});
+       
+       
+       
    }
 }
 
--- src/services/ListUserReceiverComplimentsService.ts
+++ src/services/ListUserReceiverComplimentsService.ts
@@ -1,18 +1,24 @@
+import { classToPlain } from "class-transformer";
 import { getCustomRepository } from "typeorm"
 import { ComplimentRepositories } from "../repositories/ComplimentRepositories";
 
 // TODO criar uma lista filtrada por nome e outra por categoria
 class ListUserReceiverComplimentsService{
     // Avaliação e quem enviou 
-   async execute(user_id: string){
+   async execute(user_id: string,admin?:boolean){
       const complimentRepository = getCustomRepository(ComplimentRepositories);
-      let compliments = await complimentRepository.find({
+      let compliments;
+      if(admin === true){
+         compliments = await complimentRepository.find({relations:["userSender", "userReceiver", "tag"]});
+      }else{
+        compliments = await complimentRepository.find({
           where: {
               user_receiver:user_id
           },
           relations: ["userSender", "userReceiver", "tag"]
       });
-     return mapComplimentsReceiver(compliments);
+      }
+     return (admin) ? classToPlain(mapComplimentsReceiverComplete(compliments)) : mapComplimentsReceiver(compliments);
    }
 }
 
@@ -39,6 +45,33 @@ class ListUserReceiverComplimentsService{
         }
     });
     return compliments;
+}
+
+async function mapComplimentsReceiverComplete(rating) {
+  const compliments = await rating.map((item)=>{
+      return  {  
+        message:{
+          message_id:item.id,
+          message_text:item.message,
+          created_at:item.created_at,
+          tag_name:{
+              tag_name:item.tag.name,
+              tag_id:item.tag.id
+            },
+        },
+        user_sender:{
+          user_sender_id:item.userSender.id,
+          user_sender_name:item.userSender.name,
+          user_sender_created_at: item.userSender.created_at
+        },
+        user_receiver:{
+          user_receiver_id:item.userReceiver.id,
+          user_receiver_name:item.userReceiver.name,
+          user_receiver_created_at: item.userReceiver.created_at
+        },
+      }
+  });
+  return compliments;
 }
 
 export { ListUserReceiverComplimentsService }
