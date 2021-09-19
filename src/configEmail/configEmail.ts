a4018171d74a77ba6d8fdf87bb934e4cdb9a6300
bfsantos <bfsantos@rd.com.br>
2021-09-18 22:52:40 -0300

    cleaning code

--- src/configEmail/configEmail.ts
+++ src/configEmail/configEmail.ts
@@ -2,6 +2,6 @@
 export const CONFIG = {
     HOST: 'smtp.gmail.com',
     PORT: 587,
-    USER_MAIL: 'fernandesferreirinha9@gmail.com',
-    PASSWORD_MAIL: 'bia12248665'
+    USER_MAIL: 'exemplo.email@yahoo.com',
+    PASSWORD_MAIL: 'exemploPassword123'
 }
